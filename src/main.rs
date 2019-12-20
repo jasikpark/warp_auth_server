6725db4bee7c917850cdd82b7fafb8141386f4e4
Caleb Jasik <calebjasik@jasik.xyz>
2019-12-20 12:41:44 -0600

    trying out paths

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let routes = warp::any().map(|| "Hello, World!");
+    let register = warp::path("register").map(|| "Hello from register");
+    let login = warp::path("login").map(|| "Hello from login");
+    let logout = warp::path("logout").map(|| "Hello from logout");
+    let routes = register.or(login).or(logout);
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
